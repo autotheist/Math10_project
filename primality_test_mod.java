db20c4256aa390872fb52fad107b5893468ea109
autotheist <autotheist@gmail.com>
2021-12-08 13:48:30 -0800

    Add files via upload
    I didn't know that this was a byproduct of Fermat's little theorem. I just discovered it while tinkering with the AKS algorithm. This is essentially Fermat's primality test but I didn't know that when I made it because I'd never heard of it and I didn't know it could fail and produce "pseudoprimes". It doesn't fail up to 63 which is as high as I could test on my laptop. It's not very efficient in that sense but lightning fast if it didn't require more bits than there are stars in the universe beyond a certain point.

---
+++ primality_test_mod.java
@@ -0,0 +1,48 @@
+package primality_test_mod;
+
+//import java.math.*;
+
+public class primality_test_mod {
+
+	//This algorithm I came up with after looking at the formula that the AKS algorithm is based on.
+	//The formula given was the following: n is prime iff (x+a)^n is congruent to (x^n +a)mod n 
+	//for some n > 2 and a coprime to n. I chose some examples and found that if n was prime, 
+	//a^(n-1)(mod n)=n.
+	//
+	//I used 2 as a. I didn't care if 2 wasn't coprime to even numbers since they're composite anyway.
+	//I thought I had stumbled upon some great secret of the primes heretofore unknown to mathematics.
+	//Upon further investigation and trying to understand the theorem the AKS algorithm is based on,
+	//I realized my great "discovery" was actually Fermat's little theorem and that 2 could fail as a 
+	//useful a at some point. It just didn't fail in this test because my computer can only calculate
+	//up to 2^63. This works for all numbers less than 64 though. It's very fast too. This tests all
+	//numbers up to 64 because it doesn't take that much more time to do so. It's still in the microsecond
+	//range.
+	
+			public static void main(String[] args)
+			{
+				long startTime = System.nanoTime();
+				for (int i=3; i<=63; i++) {
+					System.out.print(i + " is ");
+					
+					if (Math.pow(2, i-1)%i==1) {
+						  
+						System.out.println("\tPrime");
+					}
+					
+					else {
+						System.out.println("\tNot Prime");
+					}
+				}
+				
+				long endTime = System.nanoTime();
+			    long duration = (endTime - startTime)/1000;
+
+				
+				System.out.println("\nMicroseconds: " + duration );
+			}
+		
+
+
+	
+
+}
